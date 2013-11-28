bd8504d42a66cd467af84cd13f84f2fc9939b459
gabriele.lana <gabriele.lana@cleancode.it>
2013-11-28 22:07:14 +0100

    starting point for javascript (nodejs) language

---
+++ nodejs/gol.js
@@ -0,0 +1,4 @@
+
+module.exports.Gol = (function() {
+  return null
+})()
---
+++ nodejs/spec/gol.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect,
+    Gol = require("../gol").Gol
+
+describe("Game of Life", function() {
+  it("should be null", function() {
+    expect(Gol).to.be.a('null')
+  })
+})
